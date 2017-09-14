0011fa729c4f024061f5324c9b996b9b84ff9318
Sam Jones <sam.jones494@googlemail.com>
2017-09-14 22:31:15 +0100

    adds test to ensure all lines are retained by #parse

--- spec/file_parser_spec.rb
+++ spec/file_parser_spec.rb
@@ -2,6 +2,7 @@ require 'file_parser'
 
 describe FileParser do
   let (:file)   { double :File, :read => "uri_1\nuri_2\nuri_3" }
+
   describe '#parse' do
     it 'returns an array' do
       expect(subject.parse(file)).to be_an_instance_of(Array)
@@ -9,6 +10,10 @@ describe FileParser do
 
     it 'retains the content of the file' do
       expect(subject.parse(file)).to include("uri_1")
+    end
+
+    it 'reatins all lines of the file' do
+      expect(subject.parse(file).size).to eq(3)
     end
   end
 end
