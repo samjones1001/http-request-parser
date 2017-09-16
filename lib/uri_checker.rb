78d113c3c3d5ca9da4dda168bbcfcb7d609448c5
Sam Jones <sam.jones494@googlemail.com>
2017-09-16 09:06:22 +0100

    #is_valid_uri? checks uri structure

--- lib/uri_checker.rb
+++ lib/uri_checker.rb
@@ -1,2 +1,7 @@
 class UriChecker
+
+  def is_valid_uri?(uri)
+    valid_structure = Regexp.new(/^https?:\/\/\w+.\w+(\.\w+)?/)
+    !!(uri =~ valid_structure)
+  end
 end
--- spec/uri_checker_spec.rb
+++ spec/uri_checker_spec.rb
@@ -1,4 +1,17 @@
 require 'uri_checker'
 
 describe UriChecker do
+  describe '#is_valid_uri?' do
+    it 'returns true if provided with a valid http uri' do
+      expect(subject.is_valid_uri?('http://validuri.com')).to be(true)
+    end
+
+    it 'returns true if provided with a valid https uri' do
+      expect(subject.is_valid_uri?('https://validuri.com')).to be(true)
+    end
+
+    it 'returns false if provided with an invalid uri' do
+      expect(subject.is_valid_uri?('https://validuri.com')).to be(true)
+    end
+  end
 end
