cb0ec7abf3c229db620b25edb4b86e8d27dfc784
Sam Jones <sam.jones494@googlemail.com>
2017-09-16 10:27:08 +0100

    adds #check_for_response which returns a response object on success

--- lib/uri_checker.rb
+++ lib/uri_checker.rb
@@ -1,5 +1,10 @@
 class UriChecker
   require 'uri'
+  require 'net/http'
+
+  def check_for_response(uri)
+    Net::HTTP.get_response(URI.parse(uri))
+  end
 
   def is_valid_uri?(uri)
     uri =~ /\A#{URI::regexp(['http', 'https'])}\z/ ? true : false
--- spec/uri_checker_spec.rb
+++ spec/uri_checker_spec.rb
@@ -1,6 +1,16 @@
 require 'uri_checker'
 
 describe UriChecker do
+  before do
+    stub_request(:any, "http://www.example.com")
+  end
+
+  describe '#check_for_response' do
+      it 'gets a response from a valid url' do
+        expect(subject.check_for_response('http://www.example.com')).to be_an_instance_of(Net::HTTPOK)
+      end
+  end
+
   describe '#is_valid_uri?' do
     it 'returns true if provided with a valid http uri' do
       expect(subject.is_valid_uri?('http://validuri.com')).to be(true)
