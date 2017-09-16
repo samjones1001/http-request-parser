90cee47ff84014e80e5d7037ae065dc4480d3901
Sam Jones <sam.jones494@googlemail.com>
2017-09-16 09:08:23 +0100

    adds ternary to #is_valid_uri? for readability

--- lib/uri_checker.rb
+++ lib/uri_checker.rb
@@ -2,6 +2,6 @@ class UriChecker
 
   def is_valid_uri?(uri)
     valid_structure = Regexp.new(/^https?:\/\/\w+.\w+(\.\w+)?/)
-    !!(uri =~ valid_structure)
+    uri =~ valid_structure ? true : false
   end
 end
