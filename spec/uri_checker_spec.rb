a7f2358847cc469fb7696ef79ebff4bf079ace36
Sam Jones <sam.jones494@googlemail.com>
2017-09-16 09:35:09 +0100

    adds test for invalid uri characters to #is_valid_uri?

--- spec/uri_checker_spec.rb
+++ spec/uri_checker_spec.rb
@@ -10,8 +10,12 @@ describe UriChecker do
       expect(subject.is_valid_uri?('https://validuri.com')).to be(true)
     end
 
-    it 'returns false if provided with an invalid uri' do
+    it 'returns false if provided with an invalid uri structure' do
       expect(subject.is_valid_uri?('invaliduri.jpg')).to be(false)
+    end
+
+    it 'returns false if provided with a uri with invalid characters' do
+      expect(subject.is_valid_uri?('http://<>"#`')).to be(false)
     end
   end
 end
