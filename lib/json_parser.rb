ba2f0768d2c4ce6a538377bca58e622fd03b8e7a
Sam Jones <sam.jones494@googlemail.com>
2017-09-17 09:09:42 +0100

    adds #create_error_json

--- lib/json_parser.rb
+++ lib/json_parser.rb
@@ -5,8 +5,15 @@ class JsonParser
     response_json = {
       'url' => url,
       'status_code' => status,
-      'content_length' => headers['content-length'],
+      'content_length' => headers['content-length'].to_i,
       'date' => headers['date']
+    }.to_json
+  end
+
+  def create_error_json(url, message)
+    error_json = {
+      'url' => url,
+      'error' => message
     }.to_json
   end
 end
--- spec/json_parser_spec.rb
+++ spec/json_parser_spec.rb
@@ -15,9 +15,15 @@ describe JsonParser do
         "content-length"=>"3000",
         "connection"=>"keep-alive"
       }
-      expected_output = %({"url" : "http://example.com", "status_code" : "200", "content_length" : "3000", "date" : "Sat, 16 Sep 2017 13:53:38 GMT"})
-      expect(subject.create_response_json('http://example.com', "200", headers)).to  be_json_eql(expected_output)
+      expected_output = %({"url" : "http://example.com", "status_code" : 200, "content_length" : 3000, "date" : "Sat, 16 Sep 2017 13:53:38 GMT"})
+      expect(subject.create_response_json('http://example.com', 200, headers)).to  be_json_eql(expected_output)
     end
+  end
 
+  describe '#create_error_json' do
+    it 'returns a json object describing the error' do
+      expected_output = %({"url" : "example.com", "error" : "invalid url"})
+      expect(subject.create_error_json('example.com', 'invalid url')).to be_json_eql(expected_output)
+    end
   end
 end
